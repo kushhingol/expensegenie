6b239737da477f5d120a4b15d741b3e46bb95a78
Kush Hingol <kushhingol.kh@gmail.com>
2025-12-20 14:42:42 +0530

    feat: added middleware and updated workspace methods

--- apps/server/src/routes/workspace.routes.ts
+++ apps/server/src/routes/workspace.routes.ts
@@ -1,13 +1,22 @@
 import express, { Router } from "express";
 import { authenticateUser } from "../middlewares/auth.middleware";
 import { WorkspaceController } from "../controllers/workspace.controller";
+import { validateTemplateIdForWorkspace } from "../middlewares/validateTemplateIdForWorkspace.middleware";
 
 const router: Router = express.Router();
 
 router.post(
   "/create",
   authenticateUser,
+  validateTemplateIdForWorkspace,
   WorkspaceController.createWorkspaceController
+);
+
+router.put(
+  "/edit/:workspaceId",
+  authenticateUser,
+  validateTemplateIdForWorkspace,
+  WorkspaceController.updateWorkspaceController
 );
 
 router.delete(
